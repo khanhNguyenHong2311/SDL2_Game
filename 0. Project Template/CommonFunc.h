ee95a0542db7f2bd6c1780f7e7413cd3029620dd
NguyenHongKhanh2311 <hongkhanh2311@gmail.com>
2025-04-05 22:03:36 +0700

    First Header File

---
+++ 0. Project Template/CommonFunc.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <stdio.h>
+#include <string>
+#include <cmath>
+#include<sstream>
+#include<SDL_mixer.h>
+using namespace std;
+
+void in() {
+	cout << "1111";
+}
--- 0. Project Template/FileName.cpp
+++ 0. Project Template/FileName.cpp
@@ -1,7 +1,12 @@
-#include<iostream>
+#define SDL_MAIN_HANDLED
+
+#include"CommonFunc.h"
 using namespace std;
+	
 int main() {
 	int n;
 	cin >> n;
 	cout << n;
+	in();
+}
 // khanh dep trai 
